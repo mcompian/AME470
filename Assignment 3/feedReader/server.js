94c8882c9014f1e09db3c5c98eb15f0cdbf06593
mcompian <michael.compian@gmail.com>
2016-03-27 00:23:26 -0700

    commit

--- Assignment 3/feedReader/server.js
+++ Assignment 3/feedReader/server.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
-var hostname = process.env.HOSTNAME || '54.201.123.170';
+var hostname = process.env.HOSTNAME || '54.213.158.206';
 var port = 8080;
 var db = require('./node_modules/mongoskin').db('mongodb://user:password@127.0.0.1:27017/RSSFEED');
 
