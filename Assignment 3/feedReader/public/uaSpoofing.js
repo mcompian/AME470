1067386b00f2759407faccefc85be08cc20661a9
mcompian <michael.compian@gmail.com>
2016-04-03 20:07:38 -0700

    commit

--- Assignment 3/feedReader/public/uaSpoofing.js
+++ Assignment 3/feedReader/public/uaSpoofing.js
@@ -1,113 +1,91 @@
 var ENV = {
     device: {
-        iOS: false,
-        android: false,
-        iPad: false,
-        touchSupport: false,
-    },
-    screen: {
-        height: 0,
-        width: 0,
-        small: false
+        iOS: false
+        , android: false
+        , iPad: false
+        , touchSupport: false
+    , }
+    , screen: {
+        height: 0
+        , width: 0
+        , small: false
     }
 }
 
-
-function spoofUserAgent(){
+function spoofUserAgent() {
     var agent = navigator.userAgent.toLowerCase();
 
-    if( agent.indexOf('iphone') != -1 || agent.indexOf('ipod') != -1) {
-      ENV.device.iOS = true;
-      ENV.device.android = false;
-      ENV.device.iPad = false;
-      ENV.device.touchSupport = true;
+    if (agent.indexOf('iphone') != -1 || agent.indexOf('ipod') != -1) {
+        ENV.device.iOS = true;
+        ENV.device.android = false;
+        ENV.device.iPad = false;
+        ENV.device.touchSupport = true;
+    } else if (agent.indexOf('ipad') != -1) {
+        ENV.device.iOS = false;
+        ENV.device.android = false;
+        ENV.device.iPad = true;
+        ENV.device.touchSupport = true;
+    } else if (agent.indexOf('android') != -1) {
+        ENV.device.iOS = false;
+        ENV.device.android = true;
+        ENV.device.iPad = false;
+        ENV.device.touchSupport = true;
+    } else {
+        ENV.device.iOS = false;
+        ENV.device.android = false;
+        ENV.device.iPad = false;
+        ENV.device.touchSupport = false;
     }
 
-    else if( agent.indexOf('ipad') != -1) {
-      ENV.device.iOS = false;
-      ENV.device.android = false;
-      ENV.device.iPad = true;
-      ENV.device.touchSupport = true;
+
+
+    if (ENV.device.android) {
+        ENV.screen.height = window.innerHeight;
+        ENV.screen.width = window.innerWidth;
+    } else {
+        ENV.screen.height = $(window).height();
+        ENV.screen.width = $(window).width();
     }
 
-    else if( agent.indexOf('android') != -1) {
-      ENV.device.iOS = false;
-      ENV.device.android = true;
-      ENV.device.iPad = false;
-      ENV.device.touchSupport = true;
-    }  
-    else{
-      ENV.device.iOS = false;
-      ENV.device.android = false;
-      ENV.device.iPad = false;
-      ENV.device.touchSupport = false;
-    }
-    
-    
-    
-    if(ENV.device.android){
-      ENV.screen.height = window.innerHeight;
-      ENV.screen.width = window.innerWidth;
-    }
-    else{
-      ENV.screen.height = $(window).height();
-      ENV.screen.width = $(window).width();
-    }
-    /*
-    var realHeight;
-    var realWidth;
-    */
-    var aspectRatio = ENV.screen.width / ENV.screen.height;
-    
-
-    if (aspectRatio > 1){
-      ENV.screen.orientation = "landscape";
-    }
-    else{
-      ENV.screen.orientation = "portrait";
-    } 
-
-    if(ENV.device.touchSupport){
-      if (ENV.screen.width < 800 || ENV.screen.height < 600){
-        ENV.screen.small = true;
-      }
-      else{
+    if (ENV.device.touchSupport) {
+        if (ENV.screen.width < 1000 || ENV.screen.height < 800) {
+            ENV.screen.small = true;
+        } else {
+            ENV.screen.small = false;
+        }
+    } else {
         ENV.screen.small = false;
-      }
     }
-    else{
-      ENV.screen.small = false;
-    } 
-    
-    function assignClassNames() {
-        ENV.device = window.navigator.userAgent;
-        var cn = "";
-        if (ENV.screen.small) {
-            cn += " smallScreen"
-        }
-        else {
-            cn += " bigScreen"
-        }
-        if (ENV.device.touchSupport) {
-            cn += " touchSupport"
-        }
-        else {
-            cn += " mouseSupport"
-        }
-        if (ENV.device.iOS) {
-            cn += " iOS"
-        }
-        else{}
-        if (ENV.device.android) {
-            cn += " android"
-        }
-        else {}
-        if (ENV.device.iPad) {
-            cn += " iPad"
-        }
-        else {}
-        //do something
-        //other class names: iOS, android, iPad, touchSupport, mouseSupport
-        $(body).addClass(cn)
+
+    assignClassNames()
+}
+
+function assignClassNames() {
+    var cn = "";
+    // do something
+    if (ENV.screen.small) {
+        cn += " smallScreen";
+    } else {
+        cn += " bigScreen";
     }
-  }
+
+    if (ENV.device.touchSupport) {
+        cn += " touchSupport";
+    } else {
+        cn += " mouseSupport";
+    }
+
+    if (ENV.device.android) {
+        cn += " android";
+    }
+
+    if (ENV.device.iPad) {
+        cn += " iPad";
+    }
+
+    if (ENV.device.iOS) {
+        cn += " iOS";
+    }
+    $("body").addClass(cn)
+
+}
--- Assignment 3/feedReader/server.js
+++ Assignment 3/feedReader/server.js
@@ -8,111 +8,114 @@ var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8080;
 var db = require('./node_modules/mongoskin').db('mongodb://user:password@127.0.0.1:27017/RSSFEED');
 
+//app.use(session({secret: "This is a secret."}));
+
 app.get("/", function (req, res) {
-      res.redirect("/index.html");
+    res.redirect("/index.html");
 });
 
 app.get("/getAllPosts", function (req, res) {
     var link = decodeURIComponent(req.query.link);
-    readURL(link, function(data){
-     res.send(data); // send response body
+    readURL(link, function (data) {
+        res.send(data); // send response body
     });
 });
 
-app.get('/listSubs', function(req, res){
-      var info = req.query;
-      db.collection('subs').find({user:info.user}).toArray(function(err, result) {
-        if(err){
-          res.send("[]")
+app.get('/listSubs', function (req, res) {
+    var info = req.query;
+    db.collection('subs').find({user: info.user}).toArray(function (err, result) {
+        if (err) {
+            res.send("[]")
+        } else {
+            res.send(JSON.stringify(result));
         }
-        else{
-          res.send(JSON.stringify(result));
-        }
-      });
+    });
 });
 
 
-app.get('/deleteSub', function(req, res){
-      var info = req.query;
-      db.collection('subs').remove({user:info.user, id:info.id}, function(err, result) {
-        if(err){
-          res.send("0")
+app.get('/deleteSub', function (req, res) {
+    var info = req.query;
+    db.collection('subs').remove({
+        user: info.user
+        , id: info.id
+    }, function (err, result) {
+        if (err) {
+            res.send("0")
+        } else {
+            res.send("1");
         }
-        else{
-          res.send("1");
-        }
-      });
+    });
 });
 
-app.get('/addOrEditSub', function(req, res){
-      var info = req.query;
-      db.collection('subs').findOne({link:info.link}, function(err, result) {
-        if(result){
-          var temp = Object.keys(info);
-          var key;
-          for(var t = 0; t <  temp.length; t++){
-            key = temp[t];
-            result[key] = info[key];
-          }
-          db.collection('subs').save(result, function(err2) {
-            if (err2) {
-              res.send("0");            
-            } else {        
-              res.send("1");
-            }   
-          }); 
-        }
-        else{
-          db.collection('subs').insert(info, function(err3, r3) {
-            if (err3) {
-              res.send("0");            
+app.get('/addOrEditSub', function (req, res) {
+    var info = req.query;
+    db.collection('subs').findOne({
+        link: info.link
+    }, function (err, result) {
+        if (result) {
+            var temp = Object.keys(info);
+            var key;
+            for (var t = 0; t < temp.length; t++) {
+                key = temp[t];
+                result[key] = info[key];
             }
-             else {       
-              res.send("1");
-            }   
-          });
+            db.collection('subs').save(result, function (err2) {
+                if (err2) {
+                    res.send("0");
+                } else {
+                    res.send("1");
+                }
+            });
+        } else {
+            db.collection('subs').insert(info, function (err3, r3) {
+                if (err3) {
+                    res.send("0");
+                } else {
+                    res.send("1");
+                }
+            });
         }
-      });
-  });
-
-app.get('/createUser', function(req, res){
-  userLib.add(req, res, db);
+    });
 });
 
-app.get('/editUser', function(req, res){
-  userLib.edit(req, res, db);
+app.get('/createUser', function (req, res) {
+    userLib.add(req, res, db);
 });
 
-app.get('/changePassword', function(req, res){
-  userLib.changePassword(req, res, db);
+app.get('/editUser', function (req, res) {
+    userLib.edit(req, res, db);
 });
 
-app.get('/login', function(req, res){
-  userLib.login(req, res, db);
+app.get('/changePassword', function (req, res) {
+    userLib.changePassword(req, res, db);
 });
 
-app.get('/getUser', function(req, res){
-  userLib.get(req, res, db);
+app.get('/login', function (req, res) {
+    userLib.login(req, res, db);
+});
+
+app.get('/getUser', function (req, res) {
+    userLib.get(req, res, db);
 });
 
 function readURL(url, cb) {
-  var data = "";
-  var protocol = url.split("://")[0];
-  var request = require(protocol).get(url, function(res) {
+    var data = "";
+    var protocol = url.split("://")[0];
+    var request = require(protocol).get(url, function (res) {
 
-    res.on('data', function(chunk) {
-      data += chunk;
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+
+        res.on('end', function () {
+            console.log(JSON.parse(data))
+            cb(data);
+        })
     });
 
-    res.on('end', function() {
-      console.log(JSON.parse(data))
-      cb(data);
-    })
-  });
-
-  request.on('error', function(e) {
-    console.log("Got error: " + e.message);
-  });
+    request.on('error', function (e) {
+        console.log("Got error: " + e.message);
+    });
 }
 
 app.use(methodOverride());
